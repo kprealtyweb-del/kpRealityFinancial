7353188af932c306f677935df240e4ad477809f4
kprealtyweb-del <kprealtyweb@gmail.com>
2026-07-23 22:28:09 +0530

    updated config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import babel from "@rolldown/plugin-babel";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [
-    react(),
-    babel({
-      plugins: ["@locator/babel-jsx"],
-    }),
-  ],
-});
+export default defineConfig({ 
+    plugins: [react()],
+})
